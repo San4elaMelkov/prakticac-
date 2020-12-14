e29731b06ae3591b5e790212fb7de8aab2b58f58
san4ela.melkov <melkiy3222@gmail.com>
2020-12-14 14:51:06 +0300

    upd laba3

--- praktikaMelkov/laba3.cs
+++ praktikaMelkov/laba3.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace praktikaMelkov
 {
     class laba3
@@ -9,10 +9,12 @@ namespace praktikaMelkov
         public static void print()
         {
             string text = Melkov.Read("Введите текст");
-            string[] s = text.Split();
-            Array.Sort(s);
-            foreach(string str in s)
-                Console.WriteLine(str);
-        }   
+            Console.WriteLine(text.Split().Length);
+            if(text.Split().Length % 2==0)
+                Array.ForEach(text.Split(' ').OrderBy(x => x.Length).ToArray(), x => Console.WriteLine(x));
+            else
+                Array.ForEach(text.Split(' ').OrderByDescending(x => x.Length).ToArray(), x => Console.WriteLine(x));
+        }
+
     }
 }
---
+++ praktikaMelkov/laba4.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace praktikaMelkov
+{
+    class Class1
+    {
+    }
+}
